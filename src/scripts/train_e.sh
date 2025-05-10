ac24f988f59a085e516eac068d3b08dcbe141001
zhengxilu <3200105645@zju.edu.cn>
2025-05-10 19:15:17 +0800

    Update train_e.sh

--- src/scripts/train_e.sh
+++ src/scripts/train_e.sh
@@ -1,9 +1,8 @@
 export DEBUG_MODE="true"
 
-
-export DATA_PATH=/data/vjuicefs_ai_terminal_wl/11181401/Mobile-RFT-main/data/GUI-R1-low
-export CKPT_PATH=/data/vjuicefs_ai_terminal_wl/public_data/models/lzx/Qwen2.5-VL-GUI-R1-ground-nothink-4ep
-export SAVE_PATH=/data/vjuicefs_ai_terminal_wl/public_data/models/lzx/Qwen2.5-VL-GUI-R1-ground-dast-nothink-8ep
+export DATA_PATH=../../data/GUI-R1-low
+export CKPT_PATH=
+export SAVE_PATH=../../ckpt/Qwen2.5-VL-GUI-R1-ground-dast-4ep
 export LOG_PATH=${SAVE_PATH}"/debug_log.txt"
 export Train_PATH=${SAVE_PATH}"/train.log"
 mkdir -p $SAVE_PATH
@@ -12,13 +11,50 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=4 \
     --node_rank="0" \
     --master_addr="127.0.0.1" \
     --master_port="12346" \
-    /data/vjuicefs_ai_terminal_wl/11181401/Mobile-RFT-main/src/virft/src/open_r1/grpo_json_action_coord_dast.py \
+    /src/virft/src/open_r1/grpo_json_action_coord-dast.py \
     --output_dir ${SAVE_PATH}  \
     --model_name_or_path ${CKPT_PATH} \
-    --data_file_paths /data/vjuicefs_ai_terminal_wl/11181401/Mobile-RFT-main/data/GUI-R1-low/train_ground.json\
-    --image_folders /data/vjuicefs_ai_terminal_wl/11181401/Mobile-RFT-main/data/GUI-R1-low/train_imgs\
+    --data_file_paths /data/GUI-R1-low/train_ground.json\
+    --image_folders /data/GUI-R1-low/train_imgs\
     --dataset_name ${DATA_PATH} \
-    --deepspeed /data/vjuicefs_ai_terminal_wl/11181401/Mobile-RFT-main/src/virft/local_scripts/zero3.json \
+    --deepspeed /src/virft/local_scripts/zero3.json \
+    --max_prompt_length 1024 \
+    --per_device_train_batch_size 2 \
+    --gradient_accumulation_steps 2 \
+    --logging_steps 1 \
+    --bf16 \
+    --report_to none \
+    --max_completion_length 512 \
+    --gradient_checkpointing true \
+    --attn_implementation flash_attention_2 \
+    --max_pixels 12845056 \
+    --num_train_epochs 4 \
+    --run_name GRPO_example \
+    --save_strategy epoch \
+    --save_only_model true \
+    --num_generations 8 \
+    >> $Train_PATH 2>&1
+
+export DEBUG_MODE="true"
+
+export DATA_PATH=../../data/GUI-R1-low
+export CKPT_PATH=../../ckpt/Qwen2.5-VL-GUI-R1-ground-dast-4ep
+export SAVE_PATH=../../ckpt/Qwen2.5-VL-GUI-R1-ground-dast-nothink-8ep
+export LOG_PATH=${SAVE_PATH}"/debug_log.txt"
+export Train_PATH=${SAVE_PATH}"/train.log"
+mkdir -p $SAVE_PATH
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=4 \
+    --nnodes="1" \
+    --node_rank="0" \
+    --master_addr="127.0.0.1" \
+    --master_port="12346" \
+    /src/virft/src/open_r1/grpo_json_action_coord-nothink.py \
+    --output_dir ${SAVE_PATH}  \
+    --model_name_or_path ${CKPT_PATH} \
+    --data_file_paths /data/GUI-R1-low/train_ground.json\
+    --image_folders /data/GUI-R1-low/train_imgs\
+    --dataset_name ${DATA_PATH} \
+    --deepspeed /src/virft/local_scripts/zero3.json \
     --max_prompt_length 1024 \
     --per_device_train_batch_size 2 \
     --gradient_accumulation_steps 2 \
