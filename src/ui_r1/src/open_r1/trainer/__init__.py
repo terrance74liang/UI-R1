fe491e9e64202413f5935d7a0231141fbdd59d0e
zhengxilu <3200105645@zju.edu.cn>
2025-05-10 19:07:00 +0800

    Update __init__.py

--- src/ui_r1/src/open_r1/trainer/__init__.py
+++ src/ui_r1/src/open_r1/trainer/__init__.py
@@ -1,3 +1,4 @@
 from .grpo_trainer import Qwen2VLGRPOTrainer
 from .vllm_grpo_trainer import Qwen2VLGRPOVLLMTrainer 
-__all__ = ["Qwen2VLGRPOTrainer", "Qwen2VLGRPOVLLMTrainer"]
+from .DAST_grpo_trainer import Qwen2VLGRPOTrainer as DASTQwen2VLGRPOTrainer
+__all__ = ["Qwen2VLGRPOTrainer", "Qwen2VLGRPOVLLMTrainer", "DASTQwen2VLGRPOTrainer"]
